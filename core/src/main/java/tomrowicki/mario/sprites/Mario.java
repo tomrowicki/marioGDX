da684e3f85f013559d52b44f8902a73e2dcb6aba
Tomasz Mrowicki <tomrowicki@gmail.com>
2024-09-23 14:08:26 +0200

    Jumping and running animations

--- core/src/main/java/tomrowicki/mario/sprites/Mario.java
+++ core/src/main/java/tomrowicki/mario/sprites/Mario.java
@@ -1,29 +1,103 @@
 package tomrowicki.mario.sprites;
 
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
+import com.badlogic.gdx.utils.Array;
 import tomrowicki.mario.screens.PlayScreen;
 
 import static tomrowicki.mario.MarioBros.PPM;
 
 public class Mario extends Sprite {
 
+    public enum State {FALLING, JUMPING, STANDING, RUNNING}
+
+    private State currentState;
+    private State previousState;
+
     public World world;
     public Body b2body;
-    private TextureRegion marioStand;
+    private final TextureRegion marioStand;
+
+    private Animation<TextureRegion> marioRun;
+    private Animation<TextureRegion> marioJump;
+    private float stateTimer;
+    private boolean runningRight;
 
     public Mario(World world, PlayScreen screen) {
         super(screen.getAtlas().findRegion("little_mario"));
         this.world = world;
+
+        currentState = State.STANDING;
+        previousState = State.STANDING;
+        stateTimer = 0f;
+        runningRight = true;
+
+        Array<TextureRegion> frames = new Array<>();
+        for (int i = 1; i < 4; i++) {
+            frames.add(new TextureRegion(getTexture(), i * 16, 11, 16, 16));
+            marioRun = new Animation<>(0.2f, frames);
+        }
+        frames.clear();
+
+        for (int i = 4; i < 6; i++) {
+            frames.add(new TextureRegion(getTexture(), i * 16, 11, 16, 16));
+            marioJump = new Animation<>(0.1f, frames);
+        }
+        frames.clear();
+
         defineMario();
         marioStand = new TextureRegion(getTexture(), 1, 11, 16, 16);
         setBounds(0, 0, 16 / PPM, 16 / PPM);
         setRegion(marioStand);
     }
 
-    public void update (float dt) {
+    public void update(float dt) {
         setPosition(b2body.getPosition().x - getWidth() / 2, b2body.getPosition().y - getHeight() / 2);
+        setRegion(getFrame(dt));
+    }
+
+    public TextureRegion getFrame(float dt) {
+        currentState = getState();
+        TextureRegion region;
+        switch (currentState) {
+            case JUMPING:
+                region = marioJump.getKeyFrame(stateTimer);
+                break;
+            case RUNNING:
+                region = marioRun.getKeyFrame(stateTimer, true);
+                break;
+            case FALLING:
+            case STANDING:
+            default:
+                region = marioStand;
+                break;
+        }
+
+        if ((b2body.getLinearVelocity().x < 0 || !runningRight) && !region.isFlipX()) {
+            region.flip(true, false);
+            runningRight = false;
+        } else if ((b2body.getLinearVelocity().x >0 || runningRight) && region.isFlipX()) {
+            region.flip(true, false);
+            runningRight = true;
+        }
+        stateTimer = currentState == previousState ? stateTimer + dt : 0f;
+        previousState = currentState;
+        return region;
+    }
+
+    public State getState() {
+        if (b2body.getLinearVelocity().y > 0 || (b2body.getLinearVelocity().y < 0 &&  previousState == State.JUMPING)) {
+            return State.JUMPING;
+        } else if (b2body.getLinearVelocity().y < 0) {
+            return State.FALLING;
+        } else if (b2body.getLinearVelocity().x != 0) {
+            return State.RUNNING;
+        } else {
+            return State.STANDING;
+        }
     }
 
     public void defineMario() {
@@ -38,5 +112,13 @@ public class Mario extends Sprite {
 
         fdef.shape = shape;
         b2body.createFixture(fdef);
+
+        // The below block is fixing a bug where Mario's animation breaks when he's running over bricks and coin boxes:
+        // "Just add this extra fixture to your defineMario() method. This EdgeShape fixture acts as Mario's feet and slides smoothly across the tiles, without bumping or changing animation."
+        FixtureDef fdef2 = new FixtureDef();
+        EdgeShape feet = new EdgeShape();
+        feet.set(new Vector2(-2 / PPM, -6 / PPM), new Vector2(2 / PPM, -6 / PPM));
+        fdef2.shape = feet;
+        b2body.createFixture(fdef2);
     }
 }
